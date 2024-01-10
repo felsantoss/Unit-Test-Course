6c7cb06c3f7947f688d4d11bc2942907d505146e
Felipe Santos <felipesantossf@outlook.com>
2024-01-10 17:30:41 -0300

    Adicionar arquivos de projeto.

---
+++ MyClasses/FileProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string filename)
+        {
+            
+        }
+    }
+}
